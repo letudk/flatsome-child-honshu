11fbd11102871bc5f07253407eca1eeb7774fe4f
Le Tu <121410236+letudk@users.noreply.github.com>
2024-06-11 17:22:55 +0700

    add code mail
    code

--- functions.php
+++ functions.php
@@ -126,3 +126,16 @@ function devvn_bank_details( $order_id = '' ) {
     }
 }
 
+//* Add SMTP Email Function
+add_action( 'phpmailer_init', 'my_phpmailer_smtp' );
+function my_phpmailer_smtp( $phpmailer ) {
+    $phpmailer->isSMTP();     
+    $phpmailer->Host = SMTP_server;  
+    $phpmailer->SMTPAuth = SMTP_AUTH;
+    $phpmailer->Port = SMTP_PORT;
+    $phpmailer->Username = SMTP_username;
+    $phpmailer->Password = SMTP_password;
+    $phpmailer->SMTPSecure = SMTP_SECURE;
+    $phpmailer->From = SMTP_FROM;
+    $phpmailer->FromName = SMTP_NAME;
+}
