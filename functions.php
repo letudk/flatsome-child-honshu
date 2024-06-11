ba53216e1b25cf714055e642952d233b634131f4
Le Tu <121410236+letudk@users.noreply.github.com>
2024-06-11 13:18:21 +0700

    add code
    add code

--- functions.php
+++ functions.php
@@ -1,2 +1,10 @@
 <?php
 // Add custom Theme Functions here
+
+
+remove_action( 'woocommerce_proceed_to_checkout', 'woocommerce_button_proceed_to_checkout', 20 );
+add_action( 'woocommerce_proceed_to_checkout', 'custom_button_proceed_to_checkout', 20 );
+function custom_button_proceed_to_checkout() {
+    echo '<a href="'.esc_url(wc_get_checkout_url()).'" class="checkout-button button alt wc-forward">' .
+    __("ĐẶT HÀNG", "woocommerce") . '</a>';
+}
